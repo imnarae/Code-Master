2e53fd09c0acd6b0edf4a203d6c4ff8965976bcb
Imnarae <119841455+imnarae@users.noreply.github.com>
2023-08-16 09:49:56 +0900

    Update 실패율.py

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,3 +1,20 @@
 def solution(N, stages):
     answer = []
+    fail = {}
+    user = len(stages)
+    
+    for i in range(1, N+1):
+        cnt = stages.count(i)
+        if user != 0:
+            fail[i] = cnt / user
+        else:
+            fail[i] = 0
+            
+        user = user - cnt
+        
+    sorted_stages = sorted(fail.items(), key=lambda x: (-x[1], x[0])) 
+    
+    for stage in sorted_stages:
+        answer.append(stage[0])
+    
     return answer
